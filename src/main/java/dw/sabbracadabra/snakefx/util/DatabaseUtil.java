3cae82ff10bc706cf0f3f45b6faa128ba8e4f24d
sabbrcadabr <dawid.want11@gmail.com>
2023-10-19 18:08:33 +0200

    Updated database util class

--- src/main/java/dw/sabbracadabra/snakefx/util/DatabaseUtil.java
+++ src/main/java/dw/sabbracadabra/snakefx/util/DatabaseUtil.java
@@ -1,6 +1,28 @@
 package dw.sabbracadabra.snakefx.util;
 
+import java.sql.*;
+
 public class DatabaseUtil {
+    private static Connection connection;
 
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            connection = DriverManager.getConnection("jdbc:sqlite:highscores.db");
+            createTable(connection);
+        }
+        return connection;
+    }
 
+    private static void createTable(Connection connection) throws SQLException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet rs = metaData.getTables(null, null, "highscores", null)) {
+            if (!rs.next()) {
+                String createTableQuery = "CREATE TABLE highscores (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                                            "score INTEGER, snakeLen INTEGER, timer INTEGER)";
+                try (Statement statement = connection.createStatement()) {
+                    statement.executeUpdate(createTableQuery);
+                }
+            }
+        }
+    }
 }
