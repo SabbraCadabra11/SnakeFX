5b1cc73cc349a1eb4e9ffa9431763cf8b546ba6b
sabbrcadabr <dawid.want11@gmail.com>
2023-10-29 16:13:02 +0100

    Added title in main menu

--- src/main/java/dw/sabbracadabra/snakefx/mainMenu/MainMenuView.java
+++ src/main/java/dw/sabbracadabra/snakefx/mainMenu/MainMenuView.java
@@ -1,8 +1,11 @@
 package dw.sabbracadabra.snakefx.mainMenu;
 
 import dw.sabbracadabra.snakefx.util.factories.ButtonFactory;
+import dw.sabbracadabra.snakefx.util.factories.LabelFactory;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -13,6 +16,7 @@ public class MainMenuView extends VBox {
     private final Button highscoresButton;
     private final Button exitButton;
     public MainMenuView() {
+        Label titleLabel = LabelFactory.getLabel("Snake", Color.LIGHTGREEN, 52, FontWeight.EXTRA_BOLD);
         newGameButton = ButtonFactory.getButton("New game", FontWeight.BOLD, 40);
         highscoresButton = ButtonFactory.getButton("Highscores", FontWeight.BOLD, 40);
         exitButton = ButtonFactory.getButton("Exit", FontWeight.BOLD, 40);
@@ -21,7 +25,8 @@ public class MainMenuView extends VBox {
         setAlignment(Pos.CENTER);
         setSpacing(20);
 
-        getChildren().addAll(newGameButton, highscoresButton, exitButton);
+        getChildren().addAll(titleLabel, newGameButton, highscoresButton, exitButton);
+        VBox.setMargin(titleLabel, new Insets(0, 0, 60, 0));
     }
 
     public Button getNewGameButton() {
